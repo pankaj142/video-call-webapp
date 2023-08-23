b1247675842ecbd59bd5427997c10ececa24d876
pankaj142 <pankajagham1@gmail..com>
2023-08-24 01:15:27 +0530

    deploy
    deploy backend to vercel

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,6 @@ const dotenv = require("dotenv")
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-
 const app = express();
 
 app.use(cors()); // our routes can be accessible from anywhere (from every origin)
